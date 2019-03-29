c05735dc56dff7b52bdff91611bd201eda5d945f
Henry Koehler <henryk1229@gmail.com>
2019-03-29 17:57:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if "#{phrase}" == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  end  
   elsif "#{phrase}".upcase == TRUE
     puts "NO, NOT SINCE 1938!"
+  end
   else 
     puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
